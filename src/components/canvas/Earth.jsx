c749d546a269a620dd5ac64365e86dfc85c2dcca
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-07-26 23:20:53 -0400

    Adjusted geometry issue for 3d model

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,14 +1,25 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  const earth = useGLTF("./planet/scene.gltf");
+  const { scene, nodes, materials } = useGLTF("./planet/scene.gltf");
+
+  useEffect(() => {
+    if (scene) {
+      scene.traverse((child) => {
+        if (child.isMesh) {
+          if (child.geometry.attributes.position.array.some(isNaN)) {
+            console.error("Model contains NaN values in position attribute");
+          }
+        }
+      });
+    }
+  }, [scene]);
 
   return (
-    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+    <primitive object={scene} scale={1.7} position-y={-0.8} rotation-y={0} />
   );
 };
 
@@ -16,7 +27,7 @@ const EarthCanvas = () => {
   return (
     <Canvas
       shadows
-      frameloop='demand'
+      frameloop='always'
       dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
       camera={{
@@ -30,11 +41,11 @@ const EarthCanvas = () => {
         <OrbitControls
           autoRotate
           enableZoom={false}
+          enablePan={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
         <Earth />
-
         <Preload all />
       </Suspense>
     </Canvas>
