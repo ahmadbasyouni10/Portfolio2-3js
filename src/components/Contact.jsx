ee7f4dac25349db0adc89fc1cd6ab9354d7f7377
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-07-26 23:18:20 -0400

    changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -65,12 +65,10 @@ const Contact = () => {
   };
 
   return (
-    <div
-      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
-    >
+    <div className={`xl:mt-12 flex flex-col gap-10 overflow-hidden`}>
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
-        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+        className='w-full bg-black-100 p-8 rounded-2xl'
       >
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
@@ -125,9 +123,9 @@ const Contact = () => {
 
       <motion.div
         variants={slideIn("right", "tween", 0.2, 1)}
-        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+        className='w-full h-[350px] md:h-[550px]'
       >
-        <EarthCanvas />
+        {/* Add your content here */}
       </motion.div>
     </div>
   );
