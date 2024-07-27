2448d3c0c492b7677a9d97dc40d2435f412b49ef
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-07-27 18:42:23 -0400

    Completed projects section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,42 +1,85 @@
 import React from "react";
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
-
 import { styles } from "../styles";
 import { github } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import next from "../constants/next.png";
+import Supabase from "../constants/Supabase.png";
+import tailwind from "../constants/tailwind.png";
+import reactt from "../constants/reactjs.png";
+import python from "../constants/python.png";
+import flaskk from "../constants/Flask.svg";
+import pytorch from "../constants/pytorch.png";
+import sqlite3 from "../constants/sqlite3.svg";
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+// Define the techImages mapping
+const techImages = {
+  "Next.js": next,
+  Supabase: Supabase,
+  Tailwind: tailwind,
+  "React": reactt,
+  "Python": python,
+  "Flask": flaskk,
+  "PyTorch": pytorch,
+  "SQLite3": sqlite3,
+};
 
 const ProjectCard = ({
   index,
   name,
   description,
   tags,
-  image,
+  images,  // Change this from 'image' to 'images'
   source_code_link,
+  website_link,
 }) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+    <motion.div
+      variants={fadeIn("up", "spring", index * 0.5, 0.75)}
+      className="w-full lg:w-[90%] xl:w-[90%] mx-auto mb-20"
+    >
       <Tilt
         options={{
-          max: 45,
+          max: 25,
           scale: 1,
           speed: 450,
         }}
-        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+        className='bg-tertiary p-8 rounded-2xl w-full h-full flex flex-col shadow-xl'
       >
-        <div className='relative w-full h-[230px]'>
-          <img
-            src={image}
-            alt='project_image'
-            className='w-full h-full object-cover rounded-2xl'
-          />
+        <div className='relative w-full h-[500px] mb-6'>
+          <Slider {...settings}>
+            {images.map((img, index) => (
+              <div key={index} className="w-full h-[500px]">
+                <img
+                  src={img}
+                  alt={`project_image_${index}`}
+                  className='w-full h-full object-cover rounded-2xl'
+                />
+              </div>
+            ))}
+          </Slider>
 
-          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+          <div className='absolute top-0 right-0 m-3 card-img_hover z-10'>
             <div
               onClick={() => window.open(source_code_link, "_blank")}
-              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+              className='black-gradient w-14 h-14 rounded-full flex justify-center items-center cursor-pointer'
             >
               <img
                 src={github}
@@ -47,20 +90,48 @@ const ProjectCard = ({
           </div>
         </div>
 
-        <div className='mt-5'>
-          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
-          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+
+        <div className='flex-grow'>
+          <h3 className='text-white font-bold text-[32px] mb-4'>{name}</h3>
+          <p className='text-secondary text-[18px] leading-relaxed'>{description}</p>
         </div>
 
-        <div className='mt-4 flex flex-wrap gap-2'>
+        <div className="flex justify-center space-x-4 my-4 items-center">
           {tags.map((tag) => (
-            <p
-              key={`${name}-${tag.name}`}
-              className={`text-[14px] ${tag.color}`}
+            <div
+              key={tag.name}
+              className="rounded-lg overflow-hidden relative group"
+              style={{
+                background: "linear-gradient(180deg, #041433 0%, #010918 100%)",
+                padding: "10px",
+                transition: "transform 0.3s ease",
+              }}
+              title={tag.name}
             >
-              #{tag.name}
-            </p>
+              <div
+                className="absolute inset-0 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out"
+                style={{ background: "rgba(0, 0, 0, 0.5)" }}
+              >
+                <span className="text-white text-xs font-poppins">{tag.name}</span>
+              </div>
+              <img
+                src={techImages[tag.name]}
+                alt={tag.name}
+                width={50}
+                height={50}
+                className="group-hover:scale-110 group-hover:opacity-50 transition-transform duration-300 ease-in-out"
+              />
+            </div>
           ))}
+        </div>
+
+        <div className='flex justify-center'>
+          <button
+            onClick={() => window.open(website_link, "_blank")}
+            className="py-3 px-6 rounded-full bg-[#5e6bff] text-white font-bold text-lg hover:bg-[#4e5bff] transition-colors duration-300"
+          >
+            Website
+          </button>
         </div>
       </Tilt>
     </motion.div>
@@ -71,24 +142,23 @@ const Works = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} `}>My work</p>
-        <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
+        <h2 className={`${styles.sectionHeadText} mb-10`}>Projects</h2>
       </motion.div>
 
-      <div className='w-full flex'>
+      <div className='w-full flex mb-20'>
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
-          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+          className='mt-3 text-secondary text-[20px] max-w-4xl leading-[30px] mx-auto text-center'
         >
-          Following projects showcases my skills and experience through
+          Following projects showcase my skills and experience through
           real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
+          links to code repositories and hosted web applications. It reflects my
           ability to solve complex problems, work with different technologies,
           and manage projects effectively.
         </motion.p>
       </div>
 
-      <div className='mt-20 flex flex-wrap gap-7'>
+      <div className='flex flex-col items-center gap-y-20'>
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
@@ -97,4 +167,4 @@ const Works = () => {
   );
 };
 
-export default SectionWrapper(Works, "");
+export default SectionWrapper(Works, "projects");
