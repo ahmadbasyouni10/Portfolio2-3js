f45d6439f12cdf981da1f108ff3f615e80e07855
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-08-08 00:31:00 -0400

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
-import { About, Contact, Works, Hero, Navbar, StarsCanvas } from './components';
+import { About, Contact, Works, Hero, Navbar } from './components';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className='relative z-0 min-h-screen overflow-hidden'>
-        <StarsCanvas />
+      <div
+        className='relative z-0 min-h-screen overflow-hidden'
+        style={{ backgroundColor: 'black' }}
+      >
         <div className='relative z-10'>
           <Navbar />
           <Hero />
